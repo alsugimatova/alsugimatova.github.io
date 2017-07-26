017bbeb52695311800e5f7df57f7ad636f2aaf18
alsugimatova <gggimme@gmail.com>
2017-07-26 22:17:22 +0300

    final

--- mail.php
+++ mail.php
@@ -1,23 +1,14 @@
-<?php 
-//Принимаем постовые данные
-$userName=$_POST['userName'];
+<?php$userName=$_POST['userName'];
 $useEmail=$_POST['userEmail'];
 $user_message=$_POST['user_message'];
-//Тут указываем на какой ящик посылать письмо
-$to = "alsugimatova@yandex.ru";
-//Далее идет тема и само сообщение
-// Тема письма
-$subject = "Заявка";
-// Сообщение письма
-$message = "
+$to="alsugimatova@yandex.ru";
+$subject="Заявка";
+$message="
 Имя пользователя: ".htmlspecialchars($userName)."<br />
 Email: ".htmlspecialchars($userEmail)."<br />
 Сообщение: ".htmlspecialchars($user_message);
-
-// Отправляем письмо при помощи функции mail();
-$headers = "From: alsugimatova.github.io \r\nContent-type: text/html; charset=UTF-8 \r\n";
-mail ($to, $subject, $message, $headers);
-// Перенаправляем человека на страницу благодарности и завершаем скрипт
+$headers="From: alsugimatova.github.io \r\nContent-type: text/html; charset=UTF-8 \r\n";
+mail($to,$subject,$message,$headers);
 header('Location: thanks.html');
 exit();
- ?>
+?>
